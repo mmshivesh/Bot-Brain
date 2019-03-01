1f554a9746dff227ae2499a8469a0dde8f8687dd
Shivesh M M <mmshivesh@gmail.com>
2019-03-01 12:07:50 +0530

    Making it look good soon

--- BotBrain/EscortViewController.swift
+++ BotBrain/EscortViewController.swift
@@ -1,18 +1,31 @@
 //
-//  Escort.swift
+//  EscortViewController.swift
 //  BotBrain
 //
-//  Created by Shivesh M M on 09/02/19.
+//  Created by Shivesh M M on 01/03/19.
 //  Copyright © 2019 DOTsens. All rights reserved.
 //
 
 import UIKit
 
 class EscortViewController: UIViewController {
+
     @IBOutlet weak var importListButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         importListButton.layer.cornerRadius = 5;
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
